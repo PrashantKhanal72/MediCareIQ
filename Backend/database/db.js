0c8e51c63982a2939afee05b38ba8df9aac13334
PrashantKhanal72 <khanalprashant72@gmail.com>
2024-04-12 21:07:07 +0545

    Database config changed

--- Backend/database/db.js
+++ Backend/database/db.js
@@ -2,12 +2,23 @@
 const mysql = require("mysql2/promise");
 
 // Create a connection pool with configuration settings.
+// const pool = mysql.createPool({
+//   host: 'localhost', // MySQL server host
+//   port:'3306',
+//   user: 'root', 
+//   database: 'my_dev_db',
+//   password: '', 
+//   waitForConnections: true, // Wait for connections if all are busy
+//   connectionLimit: 10, // Maximum number of connections in the pool
+//   queueLimit: 0 // Maximum number of queued requests (0 for no limit)
+// });
+
 const pool = mysql.createPool({
-  host: 'localhost', // MySQL server host
+  host: 'bdqulp8gii8wi8ky6j3s-mysql.services.clever-cloud.com', // MySQL server host
   port:'3306',
-  user: 'root', 
-  database: 'my_dev_db',
-  password: '', 
+  user: 'ufmibzvobk9w2gfz', 
+  database: 'bdqulp8gii8wi8ky6j3s',
+  password: 'kBHNt1fiw0SY7PtbiDsC', 
   waitForConnections: true, // Wait for connections if all are busy
   connectionLimit: 10, // Maximum number of connections in the pool
   queueLimit: 0 // Maximum number of queued requests (0 for no limit)
